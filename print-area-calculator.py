3cf867a54a3af280bfa9a40786124a0f49b3237e
Gareth Farrington <gareth@waves.ky>
2021-12-07 20:43:40 -0800

    get post processor working

--- print-area-calculator.py
+++ print-area-calculator.py
@@ -1,16 +1,14 @@
+#!/usr/bin/python
 # PRUSA SLICER tool to calculate the maximum extents of a print
 # Written by Gareth Farrington
 import sys, os, re, math
 
 sourceFile = sys.argv[1]
 
-destFile = re.sub('\.gcode$', '', sourceFile)
-os.rename(sourceFile, destFile + ".print-area-calculator.bak")
-destFile = re.sub('\.gcode$', '', sourceFile)
-destFile = destFile + '.gcode'
+backupFile = sourceFile + ".print-area-calculator.bak"
 
 absolute_mode = None
-min_x, max_y, min_x, min_y = None
+max_x = max_y = min_x = min_y = None
 
 # Read the ENTIRE g-code file into memory
 with open(sourceFile, "r") as f:
@@ -32,22 +30,24 @@ with open(sourceFile, "r") as f:
                 # look for the X and Y blocks and if found update appropriate tracking variables
                 if (block[0] == 'X'):
                     x_value = float(block[1:])
-                    min_x = min(x_value or min_x, min_x)
-                    max_x = max(x_value or max_x, max_x)
+                    min_x = min(min_x or x_value, x_value)
+                    max_x = max(max_x or x_value, x_value)
                 elif (block[0] == 'Y'):
                     y_value = float(block[1:])
-                    min_y = min(y_value or min_y, min_y)
-                    max_y = max(y_value or max_y, max_y)
+                    min_y = min(min_y or y_value, y_value)
+                    max_y = max(max_y or y_value, y_value)
     
-    replacements = {
-        "$min_x$": min_x,
-        "$max_x$": max_x,
-        "$min_y$": min_y,
-        "$max_y$": max_y,
-        } # define desired replacements here
-    replacements = dict((replacements.escape(k), v) for k, v in replacements.items()) 
-    pattern = re.compile("|".join(replacements.keys()))
+replacements = {
+    "$min_x$": str(min_x),
+    "$max_x$": str(max_x),
+    "$min_y$": str(min_y),
+    "$max_y$": str(max_y),
+    } # define desired replacements here
+replacements = dict((re.escape(k), v) for k, v in replacements.items()) 
+pattern = re.compile("|".join(replacements.keys()))
+processed_gcode = pattern.sub(lambda m: replacements[re.escape(m.group(0))], "".join(lines))
 
-    # write all input lines to the output file
-    with open(destFile, "w") as of:
-        of.write(pattern.sub(lambda m: replacements[re.escape(m.group(0))], "/n".join(lines)))
+# write all input lines to the output file
+os.rename(sourceFile, backupFile)
+with open(sourceFile, "w") as of:
+    of.write(processed_gcode)
