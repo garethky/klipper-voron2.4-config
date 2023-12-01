d46f426542408f99b8c78d3d68783657177e52d3
Gareth Farrington <gareth@waves.ky>
2023-11-30 22:42:14 -0800

    Homing
    Updated config for load cell homing

--- printer_data/config/flash_mcu.sh
+++ printer_data/config/flash_mcu.sh
@@ -1,8 +1,6 @@
 sudo service klipper stop
-sleep 5
 cd klipper
 ls -la /dev/serial/by-id/*
-make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_2D002900085053424E363420-if00
-sleep 5
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_240050000F51383438343939-if00
 ls -la /dev/serial/by-id/*
 sudo service klipper start
