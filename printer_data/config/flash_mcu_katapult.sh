c7f3ed726625ca8e96c03048f339091a9782128f
Gareth Farrington <gareth@waves.ky>
2023-12-03 15:23:54 -0800

    Updates for load cell testing

--- printer_data/config/flash_mcu_katapult.sh
+++ printer_data/config/flash_mcu_katapult.sh
@@ -1,6 +1,6 @@
 sudo service klipper stop
 cd klipper
 ls -la /dev/serial/by-id/*
-make flash FLASH_DEVICE=0483:df11
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-katapult_stm32f446xx_240050000F51383438343939-if00
 ls -la /dev/serial/by-id/*
 sudo service klipper start
